4e0bc0bef1d00f75931736afb901ed8e5f322507
shivamsuyal <shivamsuyal25@gmail.com>
2024-02-16 23:03:46 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,9 +51,9 @@
     $(".owl-carousel").owlCarousel({
         items:4,
         loop:true,
-        // margin:10,
+        margin:10,
         autoplay:true,
-        // autoplayTimeout:2000,
+        autoplayTimeout:2000,
         // autoplayHoverPause:true,
         lazyLoad: true,
         // autoWidth: true,
